a9e0862e2e24dd0892137102cb0f739aaacc1329
cavidan666666 <cavidan666666@gmail.com>
2018-07-04 22:02:42 +0400

    Tic Tac Toe

---
+++ Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace XO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.yeniOyunToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.yeniOyunToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.köməkToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.haqqındaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniOyunToolStripMenuItem,
+            this.köməkToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(283, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // yeniOyunToolStripMenuItem
+            // 
+            this.yeniOyunToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.yeniOyunToolStripMenuItem1});
+            this.yeniOyunToolStripMenuItem.Name = "yeniOyunToolStripMenuItem";
+            this.yeniOyunToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.yeniOyunToolStripMenuItem.Text = "Fayl";
+            // 
+            // yeniOyunToolStripMenuItem1
+            // 
+            this.yeniOyunToolStripMenuItem1.Name = "yeniOyunToolStripMenuItem1";
+            this.yeniOyunToolStripMenuItem1.Size = new System.Drawing.Size(129, 22);
+            this.yeniOyunToolStripMenuItem1.Text = "Yeni Oyun";
+            // 
+            // köməkToolStripMenuItem
+            // 
+            this.köməkToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.haqqındaToolStripMenuItem});
+            this.köməkToolStripMenuItem.Name = "köməkToolStripMenuItem";
+            this.köməkToolStripMenuItem.Size = new System.Drawing.Size(56, 20);
+            this.köməkToolStripMenuItem.Text = "Kömək";
+            // 
+            // haqqındaToolStripMenuItem
+            // 
+            this.haqqındaToolStripMenuItem.Name = "haqqındaToolStripMenuItem";
+            this.haqqındaToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.haqqındaToolStripMenuItem.Text = "Haqqında";
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(13, 48);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(75, 75);
+            this.A1.TabIndex = 1;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.ButonClick);
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(104, 48);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(75, 75);
+            this.A2.TabIndex = 2;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.ButonClick);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(195, 129);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(75, 75);
+            this.B3.TabIndex = 3;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.ButonClick);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(13, 129);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(75, 75);
+            this.B1.TabIndex = 1;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.ButonClick);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(104, 129);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(75, 75);
+            this.B2.TabIndex = 1;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.ButonClick);
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(195, 48);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(75, 75);
+            this.A3.TabIndex = 2;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.ButonClick);
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(195, 210);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(75, 75);
+            this.C3.TabIndex = 3;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.ButonClick);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(104, 210);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(75, 75);
+            this.C2.TabIndex = 1;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.ButonClick);
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(13, 210);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(75, 75);
+            this.C1.TabIndex = 1;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.ButonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(283, 298);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem yeniOyunToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem yeniOyunToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem köməkToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem haqqındaToolStripMenuItem;
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+        }
+
+        bool yazdir = true;
+        int klikSayi = 0;
+
+        private void ButonClick(object sender, EventArgs e)
+        {
+            Button buton = (Button)sender;
+
+            if (yazdir)
+            {
+                buton.Text = "X";
+            }
+            else
+            {
+                buton.Text = "O";
+            }
+
+            yazdir = !yazdir;
+
+            //biz bura basanda yazdir false olur sonra yene basa qayidir true olur. burda hec bir deyer teyin etmir sadece cek eliyir
+
+            buton.Enabled = false;
+            klikSayi++;
+            Winner();
+        }
+
+        public void Winner()
+        {
+            bool have_Winner = false;
+
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text) && (!A1.Enabled))
+            {
+                have_Winner = true;
+            }
+            else if ((B1.Text == B2.Text) && (B2.Text == B3.Text) && (!B1.Enabled))
+            {
+                have_Winner = true;
+            }
+           else  if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && (!C1.Enabled))
+            {
+                have_Winner = true;
+            }
+
+
+            else if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && (!A1.Enabled))
+            {
+                have_Winner = true;
+            }
+            else if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && (!A2.Enabled))
+            {
+                have_Winner = true;
+            }
+            else if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && (!A3.Enabled))
+            {
+                have_Winner = true;
+            }
+
+
+
+            else if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && (!A1.Enabled))
+            {
+                have_Winner = true;
+            }
+            else if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && (!C1.Enabled))
+            {
+                have_Winner = true;
+            }
+
+
+
+
+
+            if (have_Winner == true)
+            {
+                DisableButtons();
+
+                string winner = "";
+
+                if (yazdir)
+
+                    winner = "O";
+                else
+                    winner = "X";
+
+                MessageBox.Show(winner + "win","yeah");
+
+            }
+
+            else
+            {
+                if (klikSayi == 9)
+                {
+                    MessageBox.Show("Draw", "Bummer");
+                }
+
+            }
+
+                
+            }
+           
+        
+
+        private void DisableButtons()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button b = (Button)c;
+                    b.Enabled = false;
+                }
+            }
+            catch (Exception)
+            {
+
+             
+            }
+        }
+    }
+}
